1e6bcabcb9215d80eeb50dc82235e6eea965d22e
tylernbui <tbui@yoketeam.com>
2026-02-17 00:49:03 -0800

    app module & routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./features/auth/login/login').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'flight-form',
+    loadComponent: () => import('./features/flight/flight-form/flight-form').then((m) => m.FlightFormComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'success',
+    loadComponent: () => import('./features/flight/success/success').then((m) => m.SuccessComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '/login',
+  },
+];
